a65be90857fb24541cbc06d5b2a3297e5ae300d8
Itzkuldeep <2021pietcakuldeep025@poornima.org>
2024-07-19 22:05:59 +0530

    Add missing HTML templates for about, resources, signup, contact, and login pages

--- back.py
+++ back.py
@@ -5,31 +5,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index_01.html")
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about_02.html")
 
 @app.route('/resources')
 def services():
-    return render_template("resources.html")
+    return render_template("resources_03.html")
 
 @app.route('/self-assessment')
 def self():
-    return render_template("self.html")
+    return render_template("self_04.html")
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contact_05.html")
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login_06.html")
 
 @app.route('/signup')
 def signup():
-    return render_template("signup.html")
+    return render_template("signup_06.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
